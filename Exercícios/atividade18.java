2bc69188c01a823564b527fbfba822f21a50c203
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-07-28 16:59:32 -0300

    Exercício 18 posições

---
+++ Exercícios/atividade18.java
@@ -0,0 +1,8 @@
+public class atividade18 {
+    public static void main(String[] args) {
+        int[] numeros = {18, 16, 3, 19, 1, 8, 41, 7, 38, 28};
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println("Posição " + i + ": " + numeros[i]);
+        }
+    }
+}
