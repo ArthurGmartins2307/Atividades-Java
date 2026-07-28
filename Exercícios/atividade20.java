35b33410e02344fa4d3ac7edd6748e3dd19bbce0
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-07-28 17:12:12 -0300

    Exercício 19 média dos valores do array

---
+++ Exercícios/atividade20.java
@@ -0,0 +1,11 @@
+public class atividade20 {
+    public static void main(String[] args) {
+        int[] numeros = {18, 16, 3, 19, 1, 8, 41, 7, 38, 28};
+        int soma = 0;
+        for(int i = 0; i < numeros.length; i++){
+            soma += numeros[i];
+        }
+        double media = soma/numeros.length;
+        System.out.println("Soma: " + media);
+    }
+}
