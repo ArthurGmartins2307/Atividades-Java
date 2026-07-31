f4b368d17162d40235d41849926d1038c2560ceb
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-07-30 21:40:57 -0300

    Exercício 28 corrigido

--- Exercícios/atividade28.java
+++ Exercícios/atividade28.java
@@ -1,5 +1,31 @@
-public class atividade28{
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+public class atividade28 {
+    public static void main(String[] args) {
+        Aluno28 alunos[] = new Aluno28[5];
+        alunos[0] = new Aluno28("Arthur", 18);
+        alunos[1] = new Aluno28("Ana", 15);
+        alunos[2] = new Aluno28("Isaac", 17);
+        alunos[3] = new Aluno28("Henrique", 17);
+        alunos[4] = new Aluno28("Gabriela", 19
+        );
+
+        int maisVelho = alunos[0].idade;
+        String maisVelhoNome = alunos[0].nome;
+        for (int i = 0; i < alunos.length; i++) {
+            if (alunos[i].idade > maisVelho) {
+                maisVelho = alunos[i].idade;
+                maisVelhoNome = alunos[i].nome;
+            }
+        }
+        System.out.println("Aluno mais velho: " + maisVelhoNome + " Idade: " + maisVelho);
+    }
+}
+
+class Aluno28{
+    String nome;
+    int idade;
+
+    public Aluno28(String n, int i){
+        this.nome = n;
+        this.idade = i;
     }
 }
