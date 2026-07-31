4c3131158769436240011bbb35077eb4e97fc96d
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-07-30 21:55:44 -0300

    Exercício 29 array

---
+++ Exercícios/atividade29.java
@@ -0,0 +1,31 @@
+public class atividade29 {
+    public static void main(String[] args) {
+        Aluno29 alunos[] = new Aluno29[5];
+        alunos[0] = new Aluno29("Arthur", 18);
+        alunos[1] = new Aluno29("Ana", 15);
+        alunos[2] = new Aluno29("Isaac", 17);
+        alunos[3] = new Aluno29("Henrique", 17);
+        alunos[4] = new Aluno29("Gabriela", 19
+        );
+
+        int maisNovo = alunos[0].idade;
+        String maisNovoNome = alunos[0].nome;
+        for (int i = 0; i < alunos.length; i++) {
+            if (alunos[i].idade < maisNovo) {
+                maisNovo = alunos[i].idade;
+                maisNovoNome = alunos[i].nome;
+            }
+        }
+        System.out.println("Aluno(a) mais novo(a): " + maisNovoNome + " Idade: " + maisNovo);
+    }
+}
+
+class Aluno29{
+    String nome;
+    int idade;
+
+    public Aluno29(String n, int i){
+        this.nome = n;
+        this.idade = i;
+    }
+}
