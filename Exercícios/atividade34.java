7da7aaf70c0669b125ac428636a4396c63dc43e1
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-08-05 19:55:49 -0300

    Exercício 34 array com objetos relacinados

---
+++ Exercícios/atividade34.java
@@ -0,0 +1,20 @@
+public class atividade34 {
+    
+}
+
+class Professor{
+    String nome;
+    double salario;
+
+    public Professor(String n, double s){
+        this.nome = n;
+        this.salario = s;
+    }
+}
+
+class Disciplina{
+    String nome;
+    Professor professorDisciplina;
+
+
+}
