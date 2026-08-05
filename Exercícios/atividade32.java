d213da15c8e306c13f0b3f51f800ad3402da76da
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-08-04 22:42:21 -0300

    Exercício 32 array e objetos

--- Exercícios/atividade32.java
+++ Exercícios/atividade32.java
@@ -1,3 +1,35 @@
 void main() {
-    
+    Pessoa32 pessoa = new Pessoa32("Arthur", 18);
+    Cachorro doguinho = new Cachorro("Plutão", "Labrador", pessoa);
+    doguinho.mostrarInformacoes();
 }
+
+class Pessoa32{
+    String nome;
+    int idade;
+
+    public Pessoa32(String n, int i){
+        this.nome = n;
+        this.idade = i;
+    }
+}
+
+class Cachorro{
+    String nome;
+    String raca;
+    Pessoa32 dono;
+
+    public Cachorro(String n, String r, Pessoa32 dono){
+        this.nome = n;
+        this.raca = r;
+        this.dono = dono;
+    }
+
+    public void mostrarInformacoes(){
+        System.out.println("Nome do cachorro: " + this.nome);
+        System.out.println("Raça: " + this.raca);
+        System.out.println("Nome do dono: " + dono.nome);
+        System.out.println("Idade do dono: " + dono.idade);
+    }
+}
+
