1f4eb467ea1f00d1b5180fea12146cb7b1f32ce7
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-08-05 20:07:48 -0300

    Exercício 34 array com objetos relacinados finalizado

--- Exercícios/atividade34.java
+++ Exercícios/atividade34.java
@@ -1,5 +1,9 @@
 public class atividade34 {
-    
+    public static void main(String[] args) {
+        Professor carlos = new Professor("Carlos", 15.000);
+        Disciplina fisica = new Disciplina("Física", carlos);
+        fisica.mostrarDisciplina();
+    }
 }
 
 class Professor{
@@ -16,5 +20,14 @@ class Disciplina{
     String nome;
     Professor professorDisciplina;
 
+    public Disciplina(String n, Professor professorDisciplina){
+        this.nome = n;
+        this.professorDisciplina = professorDisciplina;
+    }
 
+    public void mostrarDisciplina() {
+    System.out.println("Disciplina: " + this.nome);
+    System.out.println("Professor: " + professorDisciplina.nome);
+    System.out.println("Salário: " + professorDisciplina.salario);
+    }
 }
