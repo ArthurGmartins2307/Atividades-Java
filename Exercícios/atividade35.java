0e46566622eb446330fc458a2e63e0ad64c7cebe
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-08-06 20:10:20 -0300

    Exercício 35 arrays

--- Exercícios/atividade35.java
+++ Exercícios/atividade35.java
@@ -1,3 +1,30 @@
 public class atividade35 {
-    
+    public static void main(String[] args) {
+        Cachorro35[] cachorros = {
+            new Cachorro35("Spike", "Fox paulista"),
+            new Cachorro35("Bel", "Bulldog"),
+            new Cachorro35("Alfredo", "Pugg"),
+            new Cachorro35("Bonnie", "Golden"),
+            new Cachorro35("Tody", "Pitbull"),
+        };
+        for(int i = 0; i < cachorros.length; i++){
+            cachorros[i].mostrarCachorro();
+        }
+    }
+}
+
+class Cachorro35{
+    String nome;
+    String raca;
+
+    public Cachorro35(String n, String r){
+        this.nome = n;
+        this.raca = r;
+    }
+
+    public void mostrarCachorro(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Raça: " + this.raca);
+        System.out.println("------------------");
+    }
 }
